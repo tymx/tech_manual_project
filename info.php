192144fa53bfa302101d1b96e7a2b431e97b6126
Taylor Martinez <taylorannmartinez16@gmail.com>
2019-10-14 07:03:07 -0400

    Add files via upload

---
+++ info.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Functional Contact Form</title>
+  <link rel="stylesheet" href="../techm/style.css">
+</head>
+<body>
+  <div class="form_container" id="info">
+    <h1>Congrats First Name Last Name <br> Profile Creation a Success!</h1>
+    <p>Verify the Email Address: _________ </p>
+    <p>Check back in on the _________ for your birthday gift!</p>
+  </div>
+</body>
+</html>
